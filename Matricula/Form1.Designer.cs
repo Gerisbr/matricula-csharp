d0e83e98005d284282146f50ddd8dd98512d8073
Geris <gerisbr@hotmail.com>
2021-07-07 21:30:01 -0300

    matricula

---
+++ Matricula/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Matricula
+{
+    partial class frmMatricula
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMatricula));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtAnoNascimento = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtAnoUltimoAniversario = new System.Windows.Forms.TextBox();
+            this.txtCategoria = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnIdentificarCategoria = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // txtAnoNascimento
+            // 
+            resources.ApplyResources(this.txtAnoNascimento, "txtAnoNascimento");
+            this.txtAnoNascimento.Name = "txtAnoNascimento";
+            // 
+            // txtNome
+            // 
+            resources.ApplyResources(this.txtNome, "txtNome");
+            this.txtNome.Name = "txtNome";
+            // 
+            // txtAnoUltimoAniversario
+            // 
+            resources.ApplyResources(this.txtAnoUltimoAniversario, "txtAnoUltimoAniversario");
+            this.txtAnoUltimoAniversario.Name = "txtAnoUltimoAniversario";
+            // 
+            // txtCategoria
+            // 
+            this.txtCategoria.BackColor = System.Drawing.Color.Yellow;
+            resources.ApplyResources(this.txtCategoria, "txtCategoria");
+            this.txtCategoria.Name = "txtCategoria";
+            this.txtCategoria.ReadOnly = true;
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // btnIdentificarCategoria
+            // 
+            resources.ApplyResources(this.btnIdentificarCategoria, "btnIdentificarCategoria");
+            this.btnIdentificarCategoria.Name = "btnIdentificarCategoria";
+            this.btnIdentificarCategoria.UseVisualStyleBackColor = true;
+            this.btnIdentificarCategoria.Click += new System.EventHandler(this.btnIdentificarCategoria_Click);
+            // 
+            // frmMatricula
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.btnIdentificarCategoria);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtCategoria);
+            this.Controls.Add(this.txtAnoUltimoAniversario);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.txtAnoNascimento);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.HelpButton = true;
+            this.Name = "frmMatricula";
+            this.ShowIcon = false;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtAnoNascimento;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtAnoUltimoAniversario;
+        private System.Windows.Forms.TextBox txtCategoria;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnIdentificarCategoria;
+    }
+}
+
---
+++ Matricula/Form1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Matricula
+{
+    public partial class frmMatricula : Form
+    {
+        public frmMatricula()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIdentificarCategoria_Click(object sender, EventArgs e)
+        {
+            int anoNascimento = Convert.ToInt32( txtAnoNascimento.Text );
+            int anoUltimoAniversario = Convert.ToInt32( txtAnoUltimoAniversario.Text );
+            int idade = anoUltimoAniversario - anoNascimento;
+
+            if (idade >= 5 && idade <= 7)
+            {
+                txtCategoria.Text = "Infantil A";
+            }
+            else if (idade >= 8 && idade <= 10)
+            {
+                txtCategoria.Text = "Infantil B";
+            }
+            else if (idade >= 11 && idade <= 13)
+            {
+                txtCategoria.Text = "Juvenil A";
+            }
+            else if (idade >= 14 && idade <= 17)
+            {
+                txtCategoria.Text = "Juvenil B";
+            }
+            else if (idade >= 18)
+            {
+                txtCategoria.Text = "Adulto";
+            }
+        }
+    }
+}
